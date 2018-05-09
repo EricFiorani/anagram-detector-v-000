a9695df143f70b7dd3a04bacd467597f132c4b77
Eric Fiorani <efiorani305@gmail.com>
2018-05-09 17:58:17 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,10 +9,15 @@ class Anagram
     @matches = matches
   end
 
-  def match
-    matches.detect(%w())
+  def match(words)
+    words.select do |word|
+      is_anagram?(word)
+    end
   end
 
+  def is_anagram?(word)
+    word.chars.sort == @anagram.chars.sort
+  end
 end
 
 
